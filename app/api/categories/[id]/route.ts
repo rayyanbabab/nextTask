f843af6cfcd85c3713826585f35e13345ddf20fd
unknown <setsumsc@gmail.com>
2025-07-27 17:27:13 +0700

    Masalah Parameter

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(_req: NextRequest, context: { params: { id: string } }) {
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
   const { id } = context.params
   const categoryId = parseInt(id)
 
